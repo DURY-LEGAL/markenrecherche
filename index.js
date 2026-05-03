031771e0b19270f58675e4109254e1406b987f51
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:39:54 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// markenrecherche - Markenrecherche - Automatische Recherche in DPMA und EUIPO Datenbanken. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
